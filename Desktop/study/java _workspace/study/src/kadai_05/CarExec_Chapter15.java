88e0952b59f9d06988f3163d63e93ea00c77acf5
kwtkd <kiwatakda@gmail.com>
2024-02-12 01:12:49 +0900

    提出

---
+++ Desktop/study/java _workspace/study/src/kadai_05/CarExec_Chapter15.java
@@ -0,0 +1,12 @@
+package kadai_05;
+
+public class CarExec_Chapter15 {
+	
+	public static void main(String[] args) {
+		//オブジェクト作成
+		Car_Chapter15  myCar = new Car_Chapter15(3, 30);
+		
+		myCar.gearChange(1);
+		myCar.run();
+	}
+}
