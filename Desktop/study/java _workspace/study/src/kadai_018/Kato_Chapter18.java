a768b1eb737596da911ed17da12d41e69a5324e5
kwtkd <kiwatakda@gmail.com>
2024-02-15 08:00:57 +0900

    再提出

--- Desktop/study/java _workspace/study/src/kadai_018/Kato_Chapter18.java
+++ Desktop/study/java _workspace/study/src/kadai_018/Kato_Chapter18.java
@@ -11,6 +11,7 @@ abstract public class Kato_Chapter18 {
 	// メソッド　共通の紹介を出力する
 	public void commonIntroduce() {
 		System.out.println( "私の名前は" + familyName + givenName + "です。" );
+		System.out.println("住所は" + address + "です。" );
 	}
 	
 	// 抽象メソッド　個別の自己紹介
@@ -18,11 +19,11 @@ abstract public class Kato_Chapter18 {
 	// 抽象メソッド　フィールドの値のセット
 	abstract void setGivenName();
 	
+	
 	// メソッド　紹介を実行する
 	public void execIntroduce() {
 		System.out.println( "私の名前は" + familyName + givenName + "です。" );
 		System.out.println("住所は" + address + "です。" );
 		eachIntroduce();
-	}
-	
+	}	
 }
